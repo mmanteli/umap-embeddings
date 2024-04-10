972ef6146fd31812874bd6cf7df469e46badd062
mmanteli <amanda.myntti@gmail.com>
2024-04-10 10:47:51 +0300

    corrections

--- run_embeds.sh
+++ run_embeds.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=embeds
+#SBATCH --job-name=embeds_xlmr
 #SBATCH --account=project_2002026
-#SBATCH --time=00:30:00
+#SBATCH --time=00:15:00
 #SBATCH --partition=gpusmall
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=6G
-#SBATCH --gres=gpu:a100:1,nvme:4
+#SBATCH --mem-per-cpu=5G
+#SBATCH --gres=gpu:a100:1,nvme:3
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH -o logs/%j.out
