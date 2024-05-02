c0d1dbf8f1652da3da4ceb202aa57a3be5670fb2
mmanteli <amanda.myntti@gmail.com>
2024-05-02 10:05:46 +0300

    Correction for fold names

--- run_embeds.sh
+++ run_embeds.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=embeds
 #SBATCH --account=project_2002026
-#SBATCH --time=00:20:00
+#SBATCH --time=01:30:00
 #SBATCH --partition=gpusmall
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=3G
-#SBATCH --gres=gpu:a100:1,nvme:2
+#SBATCH --mem-per-cpu=5G
+#SBATCH --gres=gpu:a100:1,nvme:5
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH -o logs/%j.out
@@ -44,8 +44,7 @@ for lang in "${langs[@]}"; do
     srun python3 embeds.py --lang=$lang --data_name=$data_name --model_name=$model_name --fold=$fold
     #echo python3 embeds.py --lang=$lang --data_name=$data_name --model_name=$model_name --fold=$fold
 done
-exit 0
 seff $SLURM_JOBID
-mkdir -p logs/embeds_${model_name}_${fold_str}_${data_name}/${lang}/
-mv logs/${SLURM_JOBID}.* logs/embeds_${model_name}_${fold_str}_${data_name}/${lang}/
+mkdir -p logs/embeds_${model_name}_${fold}_${data_name}/${lang}/
+mv logs/${SLURM_JOBID}.* logs/embeds_${model_name}_${fold}_${data_name}/${lang}/
 exit 0
