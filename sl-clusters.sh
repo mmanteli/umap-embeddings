6374c874c64fd5840542d70c2c886655a789cc83
mmanteli <amanda.myntti@gmail.com>
2024-11-21 10:17:58 +0200

    Runners for clusters in interactive and with slurm

--- sl-clusters.sh
+++ sl-clusters.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
+#SBATCH --job-name=cluster-e5
+#SBATCH --account=project_2009199
+#SBATCH --partition=test
+#SBATCH --time=01:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH -o logs/%x.out
+#SBATCH -e logs/%x.err
 
-#model="xlmr-long-fold-1"
+module load pytorch
+model="e5"
 data="balanced_register_oscar"
 
-python clusters_mean.py --data="model_embeds/${data}/xlmr-long-fold-*/" \
-                       --langs=["en","fr","zh","ur"] \
-                       --data_name=$data \
-                       --model_name="xlmr-long" \
-                       --labels="without_MT" \
-                       --sample=2000 \
-                       --cmethod="all" \
-                       --rmethod="umap" \
-                       --n_umap="[2,6,2]" \
-                       --save_dir="metric_plots/average/${data}/xlmr-long/" \
-                       --save_prefix="mean"
+srun python clusters.py --data="model_embeds/${data}/${model}/" \
+                           --langs=["en","fr","zh","ur"] \
+                           --data_name=$data \
+                           --model_name=$model \
+                           --labels="without_MT" \
+                           --sample=5000 \
+                           --cmethod="all" \
+                           --rmethod="umap" \
+                           --n_umap="[2,10,2]" \
+                           --save_dir="metric_plots/${data}/${model}/" \
+                           --save_prefix="averaged"
+seff $SLURM_JOBID
 exit 0
+
+
+
+
+
 python clusters.py --data="model_embeds/${data}/${model}/" \
                    --langs=["en","fr","zh"] \
                    --data_name=$data \
@@ -28,7 +44,7 @@ python clusters.py --data="model_embeds/${data}/${model}/" \
                    --save_prefix="new_fig"
                    
                    #--pca_before_umap=5 \
-                   
+
 
 exit 0
 python clusters.py --data="model_embeds/veronikas/" \
@@ -43,4 +59,4 @@ python clusters.py --data="model_embeds/veronikas/" \
                    --n_umap="[2,5,1]" \
                    --save_dir="metric_plots/veronikas/" \
                    --save_prefix="fig"
-                   
+
