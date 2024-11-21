e1155a9e58462e8c7d537ff326db01ca206a99f5
mmanteli <amanda.myntti@gmail.com>
2024-11-21 13:52:37 +0200

    updated to new plotting fomat

--- run_plotting.sh
+++ run_plotting.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=plotting
 #SBATCH --account=project_2002026
-#SBATCH --time=00:15:00
-#SBATCH --partition=medium
+#SBATCH --time=00:30:00
+#SBATCH --partition=test
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=1G
@@ -15,24 +15,26 @@
 model_name=$1
 data_name=$2
 langs=$3
-wrt_column=$4
+wrt_column="preds_best"
 
-# Replace commas with hyphens for langs_hyphen
+# Replace commas with hyphens for langs_hyphen and make a list for params
 langs_hyphen=$(echo "$langs" | tr ',' '-')
-# this is for saving
+langs_list=$(echo "$langs" | sed 's/,/","/g' | awk '{print "[\""$0"\"]"}')
 
 echo "plotting ${model_name} ${dataname} with ${langs_hyphen} wrt ${wrt_column}"
 
 module load pytorch
-srun python3 plot_embed.py \
+srun python3 plot_embeddings.py \
         --embeddings="/scratch/project_2009199/umap-embeddings/model_embeds/${data_name}/${model_name}/" \
-        --languages=$langs \
+        --languages=$langs_list \
         --model_name=$model_name \
         --data_name=$data_name \
-        --use_column=$wrt_column \
-        --save_path="/scratch/project_2009199/umap-embeddings/umap-figures/${data_name}/${model_name}/${langs_hyphen}/${wrt_column}/"
+        --use_column_labels=$wrt_column \
+        --extension="html" \
+        --hover_text="text" \
+        --save_dir="/scratch/project_2009199/umap-embeddings/umap-figures/${data_name}/${model_name}/${langs_hyphen}/${wrt_column}/"
 
 seff $SLURM_JOBID
 
-mkdir -p logs/plotting_${model_name}_${dataname}/${langs_hyphen}/${wrt_column}/
-mv logs/${SLURM_JOBID}.* logs/plotting_${model_name}_${dataname}/${langs_hyphen}/${wrt_column}/
+mkdir -p logs/plotting_${model_name}_${data_name}/${langs_hyphen}/${wrt_column}/
+mv logs/${SLURM_JOBID}.* logs/plotting_${model_name}_${data_name}/${langs_hyphen}/${wrt_column}/
