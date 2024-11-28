789c91a85492605216da329423e4066b577777eb
mmanteli <amanda.myntti@gmail.com>
2024-11-28 16:30:09 +0200

    venv correction

--- sl-cluster-metrics-average.sh
+++ sl-cluster-metrics-average.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=average-clustering
 #SBATCH --account=project_462000353
-#SBATCH --partition=small
-#SBATCH --time=09:30:00
-#SBATCH --mem=32G
+#SBATCH --partition=debug
+#SBATCH --time=00:30:00
+#SBATCH --mem=36G
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
@@ -13,6 +13,7 @@
 module purge
 module use /appl/local/csc/modulefiles
 module load pytorch/2.4
+source .venv/bin/activate
 
 model="bge-m3-fold*"   # this is used to find the data
 model_name="bge-m3"    # this for naming
@@ -20,7 +21,7 @@ data="CORE"
 
 
 srun python clusters_mean.py --data="/scratch/project_462000353/amanda/register-clustering/data/model_embeds/${data}/${model}/" \
-                           --langs=["en","sv","tr"] \
+                           --langs=["en","fi","fr"] \
                            --data_name=$data \
                            --model_name=$model_name \
                            --labels="without_MT" \
