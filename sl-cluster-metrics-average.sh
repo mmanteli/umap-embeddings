10b8480e26b7f5919f222116557cf29c8b171dfc
mmanteli <amanda.myntti@gmail.com>
2024-12-10 14:38:48 +0200

    presentation slide runs

--- sl-cluster-metrics-average.sh
+++ sl-cluster-metrics-average.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=average-clustering
+#SBATCH --job-name=l-clustering
 #SBATCH --account=project_462000353
-#SBATCH --partition=debug
-#SBATCH --time=00:30:00
-#SBATCH --mem=36G
+#SBATCH --partition=small
+#SBATCH --time=18:00:00
+#SBATCH --mem=40G
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
@@ -16,18 +16,21 @@ module load pytorch/2.4
 source .venv/bin/activate
 
 model="bge-m3-fold*"   # this is used to find the data
-model_name="bge-m3"    # this for naming
-data="CORE"
+cmethod="kmeans"
+sample_per_lang=12000
+model_name="bge-m3-${sample_per_lang}-per-lang-${cmethod}"    # this for naming
+data="hplt"
 
 
 srun python clusters_mean.py --data="/scratch/project_462000353/amanda/register-clustering/data/model_embeds/${data}/${model}/" \
-                           --langs=["en","fi","fr"] \
+                           --langs=["en","fr","ur","zh"] \
                            --data_name=$data \
                            --model_name=$model_name \
                            --labels="without_MT" \
-                           --cmethod="all" \
+                           --cmethod=$cmethod \
+                           --sample=$sample_per_lang \
                            --rmethod="umap" \
-                           --n_umap="[2,10,2]" \
+                           --n_umap="[2,8,2]" \
                            --save_dir="/scratch/project_462000353/amanda/register-clustering/data/cluster_plots/averaged/${data}/${model_name}/" \
                            --save_prefix="averaged"
 
--- sl-cluster-metrics.sh
+++ sl-cluster-metrics.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=clustering
+#SBATCH --job-name=7-clustering
 #SBATCH --account=project_462000353
-#SBATCH --partition=debug
-#SBATCH --time=00:30:00
+#SBATCH --partition=small
+#SBATCH --time=05:30:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --mem=32G
+#SBATCH --mem=40G
 #SBATCH --cpus-per-task=16
 #SBATCH -o logs/%x_%j.out
 #SBATCH -e logs/%x_%j.err
@@ -15,17 +15,18 @@ module use /appl/local/csc/modulefiles
 module load pytorch/2.4
 source .venv/bin/activate
 
-model="bge-m3-fold-1"
-data="CORE"
+model="bge-m3-fold-7"
+data="hplt"
 
 echo $model $data "cluster metrics"
 srun python clusters.py --data="/scratch/project_462000353/amanda/register-clustering/data/model_embeds/${data}/${model}/" \
-                           --langs=["en","fr","fi"] \
+                           --langs=["en","fr","ur","zh"] \
                            --data_name=$data \
                            --model_name=$model \
                            --labels="without_MT" \
                            --hover_text="text" \
-                           --cmethod="kmeans" \
+                           --sample=2400 \
+                           --cmethod="all" \
                            --rmethod="umap" \
                            --n_umap="[2,4,1]" \
                            --save_dir="/scratch/project_462000353/amanda/register-clustering/data/cluster_plots/${data}/${model}/with_hover/" \
